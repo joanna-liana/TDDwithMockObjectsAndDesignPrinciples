b946a06d29223fd651fb696abb62abdfab6cf993
Joanna Liana <joanna_liana@outlook.com>
2022-12-02 21:35:41 +0100

    Add key alarm tests without changing the implementation

--- TDDMicroExercises/tire-pressure-monitoring-system/alarm.test.js
+++ TDDMicroExercises/tire-pressure-monitoring-system/alarm.test.js
@@ -3,12 +3,55 @@ const Alarm = require("./alarm");
 describe('Tyre Pressure Monitoring System', function () {
 
   describe('Alarm', function () {
+    const MIN_THRESHOLD = 16;
+    const MAX_THRESHOLD = 22;
 
-    it('Do something', function () {
+    it('is on when the pressure is too low', function () {
+      // given
+      const target = new Alarm();
 
-      var target = new Alarm();
+      target._sensor = {
+        popNextPressurePsiValue: () => {
+          return MIN_THRESHOLD;
+        }
+      };
+
+      // when
       target.check();
 
+      // then
+      expect(target.alarmOn()).toBe(true);
+    });
+
+    it('is on when the pressure is too high', function () {
+      // given
+      const target = new Alarm();
+
+      target._sensor = {
+        popNextPressurePsiValue: () => MAX_THRESHOLD
+      };
+
+      // when
+      target.check();
+
+      // then
+      expect(target.alarmOn()).toBe(true);
+    });
+
+
+    it.each([MIN_THRESHOLD + 1, MAX_THRESHOLD - 1])('is off when the pressure is within the normal range', function (pressureValue) {
+      // given
+      const target = new Alarm();
+
+      target._sensor = {
+        popNextPressurePsiValue: () => pressureValue
+      };
+
+      // when
+      target.check();
+
+      // then
+      expect(target.alarmOn()).toBe(false);
     });
 
   });
